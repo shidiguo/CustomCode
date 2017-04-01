029052f6bd6f72bbfa951048a03537f7b1d5087f
shidi guo <506460881@qq.com>
2017-04-01 15:14:37 +0800

    创建项目

---
+++ NewsTest/NewsTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewsTest
+//
+//  Created by shidi guo on 2017/4/1.
+//  Copyright © 2017年 shidi guo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NewsTest/NewsTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewsTest
+//
+//  Created by shidi guo on 2017/4/1.
+//  Copyright © 2017年 shidi guo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
